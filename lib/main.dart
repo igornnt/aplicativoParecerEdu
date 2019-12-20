3ce99008c1dcbe124d5ba9c1ed1c0947cca67a3f
Igor <igornnt@gmail.com>
2019-12-20 15:32:54 -0300

    Bibliotecas

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:parecer_app/view/HomeInicial.dart';
+import 'package:parecer_app/repositories/alunos_data_repository.dart';
+import 'package:parecer_app/ui/escola_ui/home_escola_page.dart';
+
+import 'models/avaliacao_model.dart';
 
 void main() {
+  AlunosDataRepository repository = AlunosDataRepository("6e4hMLqqvMSjdJDpY6YT", "4iioGPjlTM2UKZhKyw4r");
+  repository.buscaTodasAsAvaliacoesDosAlunos().then((resultado) => {
+    print(resultado.length)
+  });
+
   runApp(
     MaterialApp(
       home: HomeInicial(),
+      debugShowCheckedModeBanner: false,
     )
   );
 }
